486e0de01b5ea27497f142b380bce09b64dee06e
Wilson Diaz <diazw291@newschool.edu>
2016-11-12 12:55:33 -0500

    controller Script

--- Final Game/Assets/scripts/Controller.cs
+++ Final Game/Assets/scripts/Controller.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class Controller : Movement {
 	
+	public Transform obj2;
+	public float boundBreak = 1;
+	float boundDist;
 
-
-	
-	// Update is called once per frame
+	void Start() {
+		 boundDist = Vector3.Distance(transform.position, obj2.position);
+	}
+//	 Update is called once per frame
 	void Update () {
-		
+
+
 		if (Input.GetKey (KeyCode.UpArrow)) 
 
 		{
@@ -36,7 +41,7 @@ public class Controller : Movement {
 		if (Input.GetKey (KeyCode.A)) 
 
 		{
-			
+			dodge ();
 		}
 
 		if (Input.GetKey (KeyCode.S)) 
@@ -51,7 +56,53 @@ public class Controller : Movement {
 
 		}
 
+//		if (boundDist == 8) {
+//			transform.position.x = Mathf.Clamp (transform.position.x, 0f, 8);
+//		
+//		}
+
+	}
+
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Bound") 
+		{
+			speed = 6;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Bound") 
+		{
+			speed-=3;
+			Debug.Log ("wut");
+
+			if (speed <= boundBreak) 
+
+			{
+				speed = boundBreak;
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	void descend ()
+	{
+
+
 
 
 	}
+
+
 }
---
+++ Final Game/Assets/scripts/bound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bound : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		
+			Debug.Log ("trigger entered");
+
+
+
+	}
+}
