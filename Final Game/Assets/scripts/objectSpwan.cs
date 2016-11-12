f32d3337551a3e0fe219c00c11fd4eb735546183
yingtingyang <yangy389@newschool.edu>
2016-11-12 11:27:29 -0500

    new scripts

---
+++ Final Game/Assets/scripts/objectSpwan.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectSpwan : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    //public Rigidbody2D rb;
+    //public float speed;
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Spawn()
+    {
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        
+    }
+
+
+}
---
+++ Final Game/Assets/scripts/objectmove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectmove : MonoBehaviour {
+    public Vector2 velocity = new Vector2(-4, 0);
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>();
+        velocity =  new Vector2(-4,0);
+    }
+}
