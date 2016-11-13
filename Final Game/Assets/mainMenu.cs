98f6a1c6de9e46d8d706834ab97fb2af9734b031
DESKTOP\Krasniy <bellator0406@gmail.com>
2016-11-13 15:24:24 -0500

    Added credits screen & game can be started from the start screen

---
+++ Final Game/Assets/mainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartGameFeedback()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void StartGameFeedforward()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+}
---
+++ Final Game/Assets/startCredits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class startCredits : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartGameFeedback()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void StartGameFeedforward()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+}
