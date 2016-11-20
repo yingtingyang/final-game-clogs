50d30b473ec2ff5f48aa675ea982898d7d772dd5
Wilson Diaz <diazw291@newschool.edu>
2016-11-20 01:16:53 -0500

    pre-Gamejam changes

---
+++ New Unity Project/Assets/mover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour {
+	int Sp ;
+
+	void Start () {
+		Sp = 10;
+	}
+	
+	void Update () {
+	
+
+	}
+
+	public void moveUp(){
+		transform.Translate (Vector2.up * Sp * Time.deltaTime);
+	}
+	public void moveDown(){
+		transform.Translate (Vector2.down * Sp * Time.deltaTime);
+	}
+	public void moveLeft(){
+		transform.Translate (Vector2.left * Sp * Time.deltaTime);
+	}
+	public void moveright(){
+		transform.Translate (Vector2.right * Sp * Time.deltaTime);
+	}
+}
