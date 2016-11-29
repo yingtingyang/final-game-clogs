a5ee3c77d1b558fdbe6b065b26c1f02c8c3d1265
yingtingyang <yangy389@newschool.edu>
2016-11-29 16:21:03 -0500

    add health system, enemy damage, update asset visual dead function need furthur fix

---
+++ Final Game/Assets/scripts/hazarddamage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hazarddamage : MonoBehaviour {
+
+    public int attackDamage = 33;               // The amount of health taken away per attack.
+    GameObject player;                          // Reference to the player GameObject.
+    healthsystem playerHealth;                  // Reference to the player's health.
+   // Animator anim;                              // Reference to the animator component.
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<healthsystem>();
+        //anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            if (playerHealth.currentHealth > 0)
+            {
+                // ... damage the player.
+                playerHealth.TakeDamage(attackDamage);
+            }else
+            if (playerHealth.currentHealth <= 0)
+            {
+                // ... tell the animator the player is dead.
+                // anim.SetTrigger("PlayerDead");
+                Debug.Log("dead");
+            }
+        }
+    }
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
