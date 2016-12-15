d319fb546b6735fed557371730f4786f7eca243c
DESKTOP-HFGLGK0\Jimin Chung <bellator0406@gmail.com>
2016-12-15 01:33:55 -0500

    Fixed obstacle prefabs and added hit sound

---
+++ Final Game/Assets/scripts/hitSoundEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class hitSoundEffect : MonoBehaviour
+{
+    public AudioClip hit;
+    AudioSource hitSound;
+    // Use this for initialization
+    void Start()
+    {
+        hitSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.tag == "hazards")
+        {
+                hitSound.PlayOneShot(hit, 0.7F);
+            }
+
+        }
+
+    }
