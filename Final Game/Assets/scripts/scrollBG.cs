b9b0a662d65a1fdb976fd5e738710f21a952ae97
yingtingyang <yangy389@newschool.edu>
2016-12-15 15:28:37 -0500

    update scrollBG and asset

--- Final Game/Assets/scripts/scrollBG.cs
+++ Final Game/Assets/scripts/scrollBG.cs
@@ -5,7 +5,7 @@ public class scrollBG : MonoBehaviour
 {
     public float scrollSpeed = 0.5f;
     public float tileSizeZ;
-
+    public float t = 1000;
     //private Vector3 startPosition;
 
     void Start()
@@ -17,6 +17,7 @@ public class scrollBG : MonoBehaviour
     {
         Vector2 offset = new Vector2(Time.time * scrollSpeed, 0);
         GetComponent<Renderer>().material.mainTextureOffset = offset;
+        GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.yellow, t);
         //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
         //transform.position = startPosition + Vector3.left * newPosition;
     }
