e7cf1cf8193431df74390d0deaf77187e205cc02
yingtingyang <yangy389@newschool.edu>
2016-12-15 15:45:52 -0500

    color change

--- Final Game/Assets/scripts/enemyDmgP1.cs
+++ Final Game/Assets/scripts/enemyDmgP1.cs
@@ -18,9 +18,10 @@ public class enemyDmgP1 : actions
 
 		if (Input.GetKeyDown (KeyCode.B))
 		{
-            player_1.Play("dodgeP1");
+           
             duckKey = true;
-		}
+         
+        }
 		if (Input.GetKeyUp (KeyCode.B))
 		{
 			duckKey = false;
@@ -31,8 +32,9 @@ public class enemyDmgP1 : actions
 	void  OnTriggerEnter2D (Collider2D otherObj)
 	{
 		if (otherObj.transform.CompareTag ("hazards") && duckKey == true)
-		{	
-			life.lifeP1++;
+		{
+            player_1.Play("dodgeP1");
+            life.lifeP1++;
 			Debug.Log ("ducked");
 
 		} else if (otherObj.transform.CompareTag ("hazards") && duckKey == false)
--- Final Game/Assets/scripts/scrollBG.cs
+++ Final Game/Assets/scripts/scrollBG.cs
@@ -5,7 +5,7 @@ public class scrollBG : MonoBehaviour
 {
     public float scrollSpeed = 0.5f;
     public float tileSizeZ;
-    public float t = 1000;
+    public float t = 10;
     //private Vector3 startPosition;
 
     void Start()
@@ -17,7 +17,7 @@ public class scrollBG : MonoBehaviour
     {
         Vector2 offset = new Vector2(Time.time * scrollSpeed, 0);
         GetComponent<Renderer>().material.mainTextureOffset = offset;
-        GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.yellow, t);
+        //GetComponent<Renderer>().material.color = Color.Lerp(new Color(228/255,91/255,41/255),  new Color(113/255,144/255,171/255), t);
         //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
         //transform.position = startPosition + Vector3.left * newPosition;
     }
